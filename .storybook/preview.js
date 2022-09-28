20d68e345afb08f5476084f3d53a171e314550b8
Ben Singer <ben.singer@sds.co.uk>
2022-09-28 15:46:01 +0100

    storybook preview js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import {defineCustomElements} from '../loader';
+import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css';
+import 'tailwindcss/tailwind.css';
+
+defineCustomElements();
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
